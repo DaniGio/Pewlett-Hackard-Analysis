4dba8b023dac974b416939149ce08323621562d8
DaniGio <59997215+DaniGio@users.noreply.github.com>
2020-02-26 22:46:50 +1000

    adding retirement info

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 CREATE TABLE employees (
-	     emp_no INT NOT NULL,
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -31,24 +31,19 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE Dept_Emp(
+CREATE TABLE dept_emp(
   emp_no INT NOT NULL,
-  birth_date DATE NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL,
-  gender VARCHAR NOT NULL,
-  hire_date DATE NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE Titles(
   emp_no INT NOT NULL,
   tittle VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
-SELECT *
-FROM departments;
